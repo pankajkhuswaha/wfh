affaf0c32a6fd72b926ba3e56dfde6d8224ba0f8
pankajkhuswaha <khuswahapankaj00@gmail.com>
2023-12-27 18:07:29 +0530

    feat: adding Sign and login

--- frontend/src/layout/index.tsx
+++ frontend/src/layout/index.tsx
@@ -2,10 +2,10 @@
 import Header from "./Header/Header";
 const LayOut = ({ children }: ChildProps) => {
   return (
-    <>
+    <div className="w-full min-h-screen dark:text-white text-black bg-gray-200 dark:bg-[#0A0A0A]">
       <Header />
       {children}
-    </>
+    </div>
   );
 };
 
